0be868566ff21634c90b0ed7c7c49d24a6a527f7
jaidel <vctreeei@gmail.com>
2025-10-15 10:04:54 +0800

    1

--- Header.js
+++ Header.js
@@ -8,16 +8,17 @@ export default function Header() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container">
         <Link className="navbar-brand" to="/">TaskFlow</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu" aria-controls="navMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navMenu">
           <ul className="navbar-nav me-auto">
             <li className="nav-item">
-              <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} to="/">TASK LIST</Link>
+              <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} to="/">Task List</Link>
             </li>
             <li className="nav-item">
-              <Link className={`nav-link ${location.pathname === "/add" ? "active" : ""}`} to="/add">ADD TASK</Link>
+              <Link className={`nav-link ${location.pathname === "/add" ? "active" : ""}`} to="/add">Add Task</Link>
             </li>
           </ul>
         </div>
